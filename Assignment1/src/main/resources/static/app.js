b6e8d54c9cb327d90579e0122304a588364099f2
Tingye-xie <xiety004@mymail.unisa.edu.au>
2026-09-10 14:30:17 +0930

    made the stop start more intuitive and save audio in chunks

--- Assignment1/src/main/resources/static/app.js
+++ Assignment1/src/main/resources/static/app.js
@@ -2,18 +2,29 @@ const startButton = document.getElementById("startButton");
 //find the id in html to determine the behaviour for that button
 const stopButton = document.getElementById("stopButton");
 const status = document.getElementById("status");
-let microphone = null;
+let microphone;
+let recorder;
+let audio_chucks  = [];
 
 
 startButton.addEventListener("click", async() => {
 	try{ //audio:true is asking for access option
 		 microphone = await navigator.mediaDevices.getUserMedia({audio:true});
+		 recorder = new MediaRecorder(microphone);
+		 recorder.start();
 		//change the default status to recording....
-		status.textContent = "Microphone Access granted";
-		//disable start because prevent user pressing twice
+		status.textContent = "Recording";
+		//disable start because prevent user pressing twice and activate the stopbutton
 		startButton.disabled = true;
-	
 		stopButton.disabled = false;
+		
+		//reset the array here if want to save another audio.
+		audio_chunks = [];
+		
+		recorder.addEventListener("dataavailable", (event) => {
+			if(event.data.size > 0)
+				{audio_chunks.push(event.data)}
+		});
 		
 	} catch (error) {
 		status.textContent = "Microphone access was not allowed.";
@@ -24,13 +35,17 @@ stopButton.addEventListener("click", async() => {
 	
 		if(!microphone)
 			return;
-		
+		//stops the recording
+		recorder.stop();
+		//only disconnet the microphone
 		microphone.getTracks().forEach((track) => track.stop());
+		// Clears the variable.
 		microphone = null;
 		
 		status.textContent = "Microphone stopped.";
-		//set stop button to false to allow stopping
+		//after stop was pressed than enable start button to record again.
 		stopButton.disabled = true;
+        startButton.disabled = false;
 		
 	}
 );
